e1f6deeed1d26201ed6269249d86b93ed23b9c10
Xylia <xy231153nr@gmail.com>
2026-09-14 13:50:27 +0100

    Update script.js
    Fixed leap year bug

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const BirthDate=new Date("2008-10-14");
 const CurrentTime=Date.now();
 const Difference=CurrentTime-BirthDate;
 const AgeInDays=Math.floor(Difference/(1000*60*60*24));
-const Age=AgeInDays/365
+const Age=AgeInDays/365.25
 document.getElementById.textContent=Math.floor(Age);
