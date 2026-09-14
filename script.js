9aca92df9c8e8f724a5dba21bd79e9141a19fb16
Xylia <xy231153nr@gmail.com>
2026-09-14 13:46:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const CurrentTime=Date.now();
 const Difference=CurrentTime-BirthDate;
 const AgeInDays=Math.floor(Difference/(1000*60*60*24));
 const Age=AgeInDays/365
+document.getElementById.textContent=Math.floor(Age);
